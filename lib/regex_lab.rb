a5e5bcde8fe525b3a64a2e799602b10c05733096
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-19 22:48:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-  /\A[aeiou]/
+  /\A[aeiou]/.match
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  /^un.*ing$/.scan
 end
 
 def words_five_letters_long(text)
-
+  /^\w{5}/.scan
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  /^[A-Z].*\./.match
 end
 
 def valid_phone_number?(phone)
-
+  phone.gsub(/[)( -]/, "")
+  /[0-9]{10}/.match
 end
