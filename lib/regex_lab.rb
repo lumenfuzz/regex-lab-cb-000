a7a0a094b6745d0a7b236e1ab797780ead948dfd
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-19 23:09:10 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-  puts word
   return true if word.match(/\A[aeiouAEIOU]/) != nil
   return false if word.match(/\A[aeiouAEIOU]/) == nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  /^un.*ing$/.scan
+  text.scan(/^un.*ing$/)
 end
 
 def words_five_letters_long(text)
-  /^\w{5}/.scan
+  text.scan(/^\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  /^[A-Z].*\./.match
+  return true if word.match(/^[A-Z].*\./) != nil
+  return false if word.match(/^[A-Z].*\./) == nil
 end
 
 def valid_phone_number?(phone)
